c60369dfee6e0cb2786385bbf77c7d8cfa90053b
Josh Wisenbaker <josh.wisenbaker@jamf.com>
2022-12-02 16:24:25 -0500

    Initial Commit

---
+++ Stratus/StratusApp.swift
@@ -0,0 +1,17 @@
+//
+//  StratusApp.swift
+//  Stratus
+//
+//  Created by Josh Wisenbaker on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct StratusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
